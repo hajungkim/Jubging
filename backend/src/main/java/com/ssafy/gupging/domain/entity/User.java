08a9c8531fe0547aacb4c187262dea9ba0dc6f10
hajunge <haha950903@naver.com>
2021-07-23 10:37:47 +0900

    [BE] init

---
+++ backend/src/main/java/com/ssafy/gupging/domain/entity/User.java
@@ -0,0 +1,45 @@
+package com.ssafy.gupging.domain.entity;
+
+import com.ssafy.gupging.dto.UserSaveRequestDto;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    private String email;
+
+    private String nickname;
+
+    private String password;
+
+    private int score;
+
+    private String profilePath;
+
+    private int articleCount;
+
+    private LocalDateTime createdDate;
+
+    public static User saveUser(UserSaveRequestDto request) {
+        User result = new User();
+        result.email = request.getEmail();
+        result.nickname = request.getNickname();
+        result.password = request.getPassword();
+        result.createdDate = LocalDateTime.now();
+        return result;
+    }
+}
