41ea4e741ca3e9a85f368b6b0290167eef21ebc7
박선주 <cucu9823@naver.com>
2021-07-26 17:24:40 +0900

    [FE] ADD : 디테일페이지진행중

--- frontend/src/components/home/FollowList.vue
+++ frontend/src/components/home/FollowList.vue
@@ -1,15 +1,84 @@
 <template>
-  <div>
-      <button>팔로우</button>
+  <div class="follow_div">
+    <label class="switch">
+      <input type="checkbox" @click="followToggle()">
+      <span class="slider round"></span>
+    </label>
   </div>
 </template>
 
 <script>
 export default {
-
+  data(){
+    return {
+      toggle:true
+    }
+  },
+  methods:{
+    followToggle(){
+      console.log(this.toggle)
+      this.toggle = !this.toggle
+    }
+  }
 }
 </script>
 
 <style>
-
+.follow_div{
+  display: flex;
+  margin-left: auto;
+  margin-left:30px;
+  justify-content: center;
+  align-items: center;
+}
+.switch{
+  position:relative;
+  display: inline-block;
+  width:52px;
+  height:28px;
+}
+.switch input{
+  opacity: 0;
+  width: 0;
+  height: 0;
+}
+.slider{
+  position: absolute;
+  cursor: pointer;
+  top: 0;
+  left:0;
+  right:0;
+  bottom:0;
+  background-color: #ccc;
+  -webkit-transition: .4s;
+  transition: .4s;
+}
+.slider:before{
+  position: absolute;
+  content:"";
+  height: 20px;
+  width: 20px;
+  left:4px;
+  bottom:4px;
+  background-color: white;
+  -webkit-transition: .4s;
+  transition: .4s;
+}
+input:checked + .slider{
+  background-color: #2196F3;
+}
+input:focus + .slider{
+  box-shadow: 0 0 1px #2196F3;
+}
+input:checked +.slider:before{
+  -webkit-transform: translateX(26px);
+  -ms-transform: translateX(26px);
+  transform: translateX(26px);
+}
+.slider.round{
+  border-radius: 34px;
+}
+.slider.round:before{
+  border-radius: 50%;
+}
 </style>
--- frontend/src/components/home/PhotoList.vue
+++ frontend/src/components/home/PhotoList.vue
@@ -1,30 +1,81 @@
 <template>
-    <article class="location-listing">
-      <router-link :to="{name:'Detail'}">
-        <img src="@/assets/sample.png" width="180px" height="180px">
-      </router-link>
-    </article>
+  <div class="photo-grid">
+    <div class="today-jubging"></div>
+    <router-link :to="{name:'Detail'}" >
+      <img v-for="photo in photos" :key="photo.id" :src="photo.url" class="photo-img">
+    </router-link>
+  </div>
 </template>
 
 <script>
 export default {
   name:'PhotoList',
-  props:{
-    img_url:{
-      type:Object,
-    }
-  },
   data(){
-  },
-  created(){
-    console.log(this.img_url.url)
+    return{
+      photos:[
+        {
+          title:'0',
+          url:'http://placehold.it/185x185',
+        },
+        {
+          title:'1',
+          url:'http://placehold.it/185x185',
+        },
+        {
+          title:'2',
+          url:'http://placehold.it/185x185',
+        },
+        {
+          title:'3',
+          url:'http://placehold.it/185x185',
+        },
+        {
+          title:'4',
+          url:'http://placehold.it/185x185',
+        },
+        // {
+        //   title:'5',
+        //   url:'http://placehold.it/185x185',
+        // },
+        // {
+        //   title:'6',
+        //   url:"@/assets/sample6.png",
+        // },
+        // {
+        //   title:'7',
+        //   url:"@/assets/sample7.png",
+        // },
+        // {
+        //   title:'8',
+        //   url:"@/assets/sample8.png",
+        // },
+        // {
+        //   title:'9',
+        //   url:"@/assets/sample8.png",
+        // },
+      ]
+    }
   },
 }
 </script>
 
 <style>
-.location-listing{
-  margin-left:10px;
-  padding: 5px;
+.today-jubging {
+  width: 185px;
+  height: 90px;
+}
+
+.photo-grid {
+  column-count: 2;
+  column-gap: 0px;
+  margin-left: 14px;
+}
+
+.photo-img {
+  display: inline-block;
+  width: 185px;
+  height: 185px;
+  border-radius: 10%;
+  margin-bottom: 14px;
 }
 </style>
