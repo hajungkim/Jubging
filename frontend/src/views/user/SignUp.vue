0948eb947c44d0d08f99a9b12631c3258de35da6
daeun <zerozero7bang@gmail.com>
2021-07-26 10:57:22 +0900

    [FE] add : signup 중간

--- frontend/src/views/user/SignUp.vue
+++ frontend/src/views/user/SignUp.vue
@@ -1,18 +1,17 @@
 <template>
-  <div>
-		<label for="email">email</label>
-		<input type="text" id="email" v-model="credentials.email">
-			
-		<label for="password">password</label>
-		<input type="password" id="password" v-model="credentials.password">
+  <div class="signup-wrap">
 
-		<label for="passwordConfirmation">passwordConfirmation</label>
-		<input type="password" id="passwordConfirmation" v-model="credentials.passwordConfirmation">
+		<h1>회원가입</h1>
+		
+		<div class="from-group">
 
-		<label for="nickname">nickname</label>
-		<input type="text" id="nickname" v-model="credentials.nickname">
+			<input class="from-input" type="text" id="email" v-model="credentials.email" placeholder="email">
+			<input class="from-input" type="password" id="password" v-model="credentials.password" placeholder="password">
+			<input class="from-input" type="password" id="passwordConfirmation" v-model="credentials.passwordConfirmation" placeholder="password Confirmation">
+			<input class="from-input" type="text" id="nickname" v-model="credentials.nickname" placeholder="nickname">
 
-		<button @click="signup">Signup</button>
+			<button class="from-btn" @click="signup">Signup</button>
+		</div>
   </div>
 </template>
 
@@ -40,5 +39,43 @@ export default {
 </script>
 
 <style>
+
+.signup-wrap {
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+}
+
+.from-group {
+	display: flex;
+	flex-direction: column;
+	margin-top: 10%
+}
+
+.from-group > .from-input {
+	width: 300px;
+	height: 40px;
+	background: rgba(179, 179, 179, 0.55);
+	border: 0px;
+	border-radius: 20px;
+	padding: 0px 20px;
+	margin-bottom: 14px;
+	color: white;
+}
+
+.from-group > .from-btn {
+	color: white;
+	background: linear-gradient(278.02deg, #1CA592 -0.77%, #FFEAC1 114.65%);
+
+	width: 340px;
+	height: 40px;
+	border: 0px;
+	border-radius: 20px;
+}
+
+.text-decoration-none {
+	text-decoration: none;
+}
+
 
 </style>
