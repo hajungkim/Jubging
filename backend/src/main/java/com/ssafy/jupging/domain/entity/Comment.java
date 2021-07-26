47c25ad111cd21ccf0762fb6e1d7ec30d0b216c0
lee.sujeong10111 <lee.sujeong10111@gmail.com>
2021-07-26 22:04:36 +0900

    [BE] ADD : 댓글 조회 추가

--- backend/src/main/java/com/ssafy/jupging/domain/entity/Comment.java
+++ backend/src/main/java/com/ssafy/jupging/domain/entity/Comment.java
@@ -1,11 +1,13 @@
 package com.ssafy.jupging.domain.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.ssafy.jupging.dto.CommentSaveRequestDto;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.ManyToAny;
 
 import javax.persistence.*;
 import java.sql.Timestamp;
@@ -17,20 +19,24 @@ import java.sql.Timestamp;
 public class Comment {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int commentId;
+    private Long commentId;
 
     @CreationTimestamp
     @Column(nullable = false)
     private Timestamp createdDate;
 
     @Column(nullable = false)
-    private int articleId;
+    private Long articleId;
 
-    @Column(nullable = false)
-    private int userId;
+    @Column(updatable = false)
+    private Long userId;
 
     @Column(length = 500 , nullable = false)
     private String commentContent;
+
+    @ManyToOne
+    @JoinColumn(name = "userId", insertable = false, updatable = false)
+    private User user;
 
     public static Comment saveComment(CommentSaveRequestDto request) {
         Comment comment = new Comment();
