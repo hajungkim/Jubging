eb772aacd1a08afecbbc734370de3c78f706e19c
hajunge <haha950903@naver.com>
2021-07-26 15:12:19 +0900

    [BE] ADD : 팔로우 저장, 팔로우/팔로워 리스트 반환

---
+++ backend/src/main/java/com/ssafy/jupging/service/FollowService.java
@@ -0,0 +1,31 @@
+package com.ssafy.jupging.service;
+
+import com.ssafy.jupging.domain.entity.Follow;
+import com.ssafy.jupging.domain.repository.FollowRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
+public class FollowService {
+
+    private final FollowRepository followRepository;
+
+    @Transactional
+    public void saveFollow(Follow follow) {
+        followRepository.save(follow);
+    }
+
+    @Transactional
+    public List<Follow> findFollow(Long userId) {
+        return followRepository.findByUserId(userId);
+    }
+
+    @Transactional
+    public List<Follow> findFollower(Long userId) {
+        return followRepository.findByFollowUserId(userId);
+    }
+}
