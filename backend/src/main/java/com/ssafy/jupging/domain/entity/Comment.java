e0b2d40184abd13c0554e7f1c73156fa4823e418
lee.sujeong10111 <lee.sujeong10111@gmail.com>
2021-07-26 16:06:01 +0900

    [BE] ADD : 댓글 등록, 삭제, 개수카운팅 기능 추가

---
+++ backend/src/main/java/com/ssafy/jupging/domain/entity/Comment.java
@@ -0,0 +1,42 @@
+package com.ssafy.jupging.domain.entity;
+
+import com.ssafy.jupging.dto.CommentSaveRequestDto;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int commentId;
+
+    @CreationTimestamp
+    @Column(nullable = false)
+    private Timestamp createdDate;
+
+    @Column(nullable = false)
+    private int articleId;
+
+    @Column(nullable = false)
+    private int userId;
+
+    @Column(length = 500 , nullable = false)
+    private String commentContent;
+
+    public static Comment saveComment(CommentSaveRequestDto request) {
+        Comment comment = new Comment();
+        comment.articleId=request.getArticleId();
+        comment.userId=request.getUserId();
+        comment.commentContent=request.getCommentContent();
+        return comment;
+    }
+}
