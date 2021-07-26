a135a1e1cad9afaa4c5e2f72dc4186ed4df874f2
hajunge <haha950903@naver.com>
2021-07-26 16:43:36 +0900

    [BE] ADD : 팔로우 삭제

--- backend/src/main/java/com/ssafy/jupging/controller/FollowController.java
+++ backend/src/main/java/com/ssafy/jupging/controller/FollowController.java
@@ -1,10 +1,15 @@
 package com.ssafy.jupging.controller;
 
 import com.ssafy.jupging.domain.entity.Follow;
+import com.ssafy.jupging.domain.entity.User;
+import com.ssafy.jupging.dto.FollowResponseDto;
+import com.ssafy.jupging.dto.FollowerResponseDto;
 import com.ssafy.jupging.service.FollowService;
+import com.ssafy.jupging.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RequiredArgsConstructor
@@ -13,13 +18,14 @@ import java.util.List;
 public class FollowController {
 
     private final FollowService followService;
+    private final UserService userService;
 
     @PostMapping("/save")
     public ControllerResponse saveFollow(@RequestParam Long userId, @RequestParam Long followUserId) {
         ControllerResponse response = null;
         try {
             Follow follow = new Follow();
-            follow.saveFollow(userId, followUserId);
+            follow = follow.saveFollow(userId, followUserId);
             followService.saveFollow(follow);
 
             response = new ControllerResponse("success", "팔로우 등록 성공");
@@ -29,8 +35,8 @@ public class FollowController {
         return response;
     }
 
-    //개인피드에 자신이 팔로우하는 리스트 반환하기
-    @GetMapping("/findFollow/{id}")
+    //개인피드에 자신이 팔로우하는 리스트(팔로우 유저 아이디, 팔로우 닉네임) 반환하기
+    @GetMapping("/findfollow/{id}")
     public ControllerResponse findFollow(@PathVariable("id") Long userId) {
         ControllerResponse response = null;
         try {
@@ -38,7 +44,15 @@ public class FollowController {
             if (followList.isEmpty()) {
                 response = new ControllerResponse("success", null);
             } else {
-                response = new ControllerResponse("success", followList);
+                List<FollowResponseDto> followResponseDtoList = new ArrayList<>();
+                for (Follow foll : followList) {
+                    User user = userService.findUser(foll.getFollowUserId());
+                    FollowResponseDto followResponseDto = new FollowResponseDto();
+                    followResponseDto.saveIdAndNick(user.getUserId(), user.getNickname());
+                    followResponseDtoList.add(followResponseDto);
+                }
+
+                response = new ControllerResponse("success", followResponseDtoList);
             }
         } catch (Exception e) {
             response = new ControllerResponse("fail", e.getMessage());
@@ -46,8 +60,8 @@ public class FollowController {
         return response;
     }
 
-    //개인피드에 자신의 팔로워 리스트 반환하기
-    @GetMapping("/findFollower/{id}")
+    //개인피드에 자신의 팔로워 리스트(팔로워 유저 아이디, 팔로워 닉네임) 반환하기
+    @GetMapping("/findfollower/{id}")
     public ControllerResponse findFollower(@PathVariable("id") Long userId) {
         ControllerResponse response = null;
         try {
@@ -55,10 +69,31 @@ public class FollowController {
             if (followerList.isEmpty()) {
                 response = new ControllerResponse("success", null);
             } else {
-                response = new ControllerResponse("success", followerList);
+                List<FollowerResponseDto> followerResponseDtoList = new ArrayList<>();
+                for (Follow foll : followerList) {
+                    User user = userService.findUser(foll.getUserId());
+                    FollowerResponseDto followerResponseDto = new FollowerResponseDto();
+                    followerResponseDto.saveIdAndNick(user.getUserId(), user.getNickname());
+                    followerResponseDtoList.add(followerResponseDto);
+                }
+
+                response = new ControllerResponse("success", followerResponseDtoList);
             }
         } catch (Exception e) {
             response = new ControllerResponse("fail", e.getMessage());
+        }
+        return response;
+    }
+
+    @DeleteMapping("/delete")
+    public ControllerResponse deleteFollow(@RequestParam Long userId, @RequestParam Long followUserId) {
+        ControllerResponse response = null;
+        try {
+            followService.deleteByUserIdAndFollowUserId(userId, followUserId);
+
+            response = new ControllerResponse("success", "팔로우 삭제 성공");
+        } catch (Exception e) {
+            response = new ControllerResponse("fail", "팔로우 삭제 실패");
         }
         return response;
     }
