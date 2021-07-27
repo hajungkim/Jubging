989c11c5728785de09fe3710e14f52d833a1c1da
daeun <zerozero7bang@gmail.com>
2021-07-27 15:43:39 +0900

    [FE] UPDATE : user ranking css(추후 리팩터링)

--- frontend/src/components/ranking/UserRank.vue
+++ frontend/src/components/ranking/UserRank.vue
@@ -1,22 +1,43 @@
 <template>
   <div>
-    <div>
-			<div class="gold">
+    <div class="top3-group">
+			<div class="top3">
+				<div>메달</div>
+				<img :src="top20[1].profile_path">
+				<span>{{ top20[1].nickname }}</span>
+				<span>{{ top20[1].score }}</span>
+			</div>
+
+			<div class="top3">
 				<div>메달</div>
 				<img :src="top20[0].profile_path">
 				<span>{{ top20[0].nickname }}</span>
 				<span>{{ top20[0].score }}</span>
 			</div>
+
+			<div class="top3">
+				<div>메달</div>
+				<img :src="top20[2].profile_path">
+				<span>{{ top20[2].nickname }}</span>
+				<span>{{ top20[2].score }}</span>
+			</div>
+
 		</div>
 		
 		<hr>
 
-		<div class="top20-wrap">
-			<div v-for="(user, idx) in top20" :key="idx" class="top20">
-				<span>{{ idx }}</span>
-				<img :src="user.profile_path">
-				<span>{{ user.nickname }}</span>
-				<span>{{ user.score }}</span>
+		<div class="top20-group">
+			<div v-for="(user, idx) in top20" :key="idx">
+				<div v-if="idx > 2" class="top20">
+					<div class="rank-user-group">
+						<span class="top20-rank">{{ idx }}</span>
+						<div class="top20-user">
+							<img :src="user.profile_path">
+							<span>{{ user.nickname }}</span>
+						</div>
+					</div>
+					<span class="top20-score">{{ user.score }}</span>
+				</div>
 			</div>
 		</div>
   </div>
@@ -58,6 +79,31 @@ export default {
 					score: '320',
 					profile_path: 'http://placehold.it/185x185',
 				},
+				{
+					nickname: 'daeun',
+					score: '320',
+					profile_path: 'http://placehold.it/185x185',
+				},
+				{
+					nickname: 'daeun',
+					score: '320',
+					profile_path: 'http://placehold.it/185x185',
+				},
+				{
+					nickname: 'daeun',
+					score: '320',
+					profile_path: 'http://placehold.it/185x185',
+				},
+				{
+					nickname: 'daeun',
+					score: '320',
+					profile_path: 'http://placehold.it/185x185',
+				},
+				{
+					nickname: 'daeun',
+					score: '320',
+					profile_path: 'http://placehold.it/185x185',
+				},
 			]
 		}
 	}
@@ -65,8 +111,12 @@ export default {
 </script>
 
 <style scoped>
+.top3-group {
+	display: flex;
+	justify-content: space-evenly;
+}
 
-.gold {
+.top3 {
 	width: 100px;
 	display: flex;
 	flex-direction: column;
@@ -74,20 +124,49 @@ export default {
 	align-items: center;
 }
 
+.top3 > img {
+	width: 80px;
+	height: 80px;
+	border-radius: 50%;
+}
 
-.top20-wrap {
+
+.top20-group {
 	padding: 5px 20px;
 }
 
 .top20 {
 	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	margin-bottom: 20px;
+}
+
+.rank-user-group {
+	display: flex;
+	justify-content: center;
 	align-items: center;
 }
 
-.top20 > img {
+.top20-rank {
+	width: 50px;
+	text-align: center;
+}
+
+.top20-user {
+	display: flex;
+	align-items: center;
+	
+}
+
+.top20-user > img {
 	width: 50px;
 	height: 50px;
 	border-radius: 50%;
+}
+
+.top20-score {
+	margin-right: 20px;
 }
 
 </style>
