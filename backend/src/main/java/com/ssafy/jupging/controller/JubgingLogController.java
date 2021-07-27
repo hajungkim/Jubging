2b647b94c0a4bfcdbae21435e9d2fa4186aff561
hajunge <haha950903@naver.com>
2021-07-27 15:54:52 +0900

    [BE] ADD : 파일 생성

--- backend/src/main/java/com/ssafy/jupging/controller/JubgingLogController.java
+++ backend/src/main/java/com/ssafy/jupging/controller/JubgingLogController.java
@@ -1,4 +1,28 @@
 package com.ssafy.jupging.controller;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.sql.Timestamp;
+
+@Getter
+@NoArgsConstructor
+@Entity
 public class JubgingLogController {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long jubgingId;
+
+    private String totalTime;
+
+    private String distance;
+
+    private Long userId;
+
+    private Timestamp createdDate;
+
 }
---
+++ backend/src/main/java/com/ssafy/jupging/domain/repository/JubgingLogRepository.java
@@ -0,0 +1,7 @@
+package com.ssafy.jupging.domain.repository;
+
+import com.ssafy.jupging.domain.entity.JubgingLog;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface JubgingLogRepository extends JpaRepository<JubgingLog, Long> {
+}
