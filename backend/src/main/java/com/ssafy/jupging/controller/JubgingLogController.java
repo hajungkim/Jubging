4a2c96b0e593917772595fd8b8597b6521ff730b
hajunge <haha950903@naver.com>
2021-07-28 13:38:15 +0900

    [BE] ADD : 줍깅로그 저장,조회,삭제

--- backend/src/main/java/com/ssafy/jupging/controller/JubgingLogController.java
+++ backend/src/main/java/com/ssafy/jupging/controller/JubgingLogController.java
@@ -1,28 +1,61 @@
 package com.ssafy.jupging.controller;
 
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import com.ssafy.jupging.domain.entity.JubgingLog;
+import com.ssafy.jupging.dto.JubgingLogSaveRequestDto;
+import com.ssafy.jupging.service.JubgingLogService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import java.sql.Timestamp;
+import java.util.List;
 
-@Getter
-@NoArgsConstructor
-@Entity
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/jubginglog")
 public class JubgingLogController {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long jubgingId;
 
-    private String totalTime;
+    private final JubgingLogService jubgingLogService;
 
-    private String distance;
+    @PostMapping
+    public ControllerResponse saveJubgingLog(@RequestBody JubgingLogSaveRequestDto requestDto) {
+        ControllerResponse response = null;
+        try {
+            JubgingLog jubgingLog = new JubgingLog();
+            jubgingLog = jubgingLog.saveJubgingLog(requestDto);
+            jubgingLogService.saveJubgingLog(jubgingLog);
 
-    private Long userId;
+            response = new ControllerResponse("success", "줍깅로그 저장 성공");
+        } catch (Exception e) {
+            response = new ControllerResponse("fail", e.getMessage());
+        }
+        return response;
+    }
 
-    private Timestamp createdDate;
+    @GetMapping("/{id}")
+    public ControllerResponse findUserJubgingLog(@PathVariable("id") Long userId) {
+        ControllerResponse response = null;
+        try {
+            List<JubgingLog> list = jubgingLogService.findUserJubgingLog(userId);
+            if (list.isEmpty()) {
+                response = new ControllerResponse("success", null);
+            } else {
+                response = new ControllerResponse("success", list);
+            }
+        } catch (Exception e) {
+            response = new ControllerResponse("fail", e.getMessage());
+        }
+        return response;
+    }
+
+    @DeleteMapping("/{id}")
+    public ControllerResponse deleteJubgingLog(@PathVariable("id") Long jubgingId) {
+        ControllerResponse response = null;
+        try {
+            jubgingLogService.deleteJubgingLog(jubgingId);
+            response = new ControllerResponse("success", "줍깅로그 삭제 성공");
+        } catch (Exception e) {
+            response = new ControllerResponse("fail", e.getMessage());
+        }
+        return response;
+    }
 
 }
--- backend/src/main/java/com/ssafy/jupging/domain/repository/JubgingLogRepository.java
+++ backend/src/main/java/com/ssafy/jupging/domain/repository/JubgingLogRepository.java
@@ -3,5 +3,9 @@ package com.ssafy.jupging.domain.repository;
 import com.ssafy.jupging.domain.entity.JubgingLog;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface JubgingLogRepository extends JpaRepository<JubgingLog, Long> {
+
+    List<JubgingLog> findByUserId(Long userId);
 }
