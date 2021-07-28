d773e96f71b48fbd72f9d1104d91a894f50ebbea
hajunge <haha950903@naver.com>
2021-07-28 14:45:23 +0900

    [BE] ADD : 미션 틀잡기

---
+++ backend/src/main/java/com/ssafy/jupging/dto/MissionSaveResponseDto.java
@@ -0,0 +1,13 @@
+package com.ssafy.jupging.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class MissionSaveResponseDto {
+
+    private Long userId;
+
+
+}
