c4dc147f30cb49423f8735746cc5d27f47697fa7
hajunge <haha950903@naver.com>
2021-07-28 13:38:15 +0900

    [BE] ADD : 줍깅로그 저장,조회,삭제

--- backend/src/main/java/com/ssafy/jupging/domain/entity/JubgingLog.java
+++ backend/src/main/java/com/ssafy/jupging/domain/entity/JubgingLog.java
@@ -1,4 +1,39 @@
 package com.ssafy.jupging.domain.entity;
 
+import com.ssafy.jupging.dto.JubgingLogSaveRequestDto;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor
+@Entity
 public class JubgingLog {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long jubgingId;
+
+    private String totalTime;
+
+    private String distance;
+
+    private Long userId;
+
+    private LocalDateTime createdDate;
+
+    public JubgingLog saveJubgingLog(JubgingLogSaveRequestDto requestDto) {
+        JubgingLog result = new JubgingLog();
+        result.totalTime = requestDto.getTotalTime();
+        result.distance = requestDto.getDistance();
+        result.userId = requestDto.getUserId();
+        result.createdDate = LocalDateTime.now();
+        return result;
+    }
 }
