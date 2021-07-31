ec9cfd1952843470339addd64497188c36a37ad4
lee.sujeong10111 <lee.sujeong10111@gmail.com>
2021-07-31 10:45:18 +0900

    [BE] ADD : 팔로우한 사람 게시글 찾기

--- backend/src/main/java/com/ssafy/jupging/controller/FollowController.java
+++ backend/src/main/java/com/ssafy/jupging/controller/FollowController.java
@@ -1,9 +1,12 @@
 package com.ssafy.jupging.controller;
 
+import com.ssafy.jupging.domain.entity.Article;
 import com.ssafy.jupging.domain.entity.Follow;
 import com.ssafy.jupging.domain.entity.User;
+import com.ssafy.jupging.dto.ArticleResponseDto;
 import com.ssafy.jupging.dto.FollowResponseDto;
 import com.ssafy.jupging.dto.FollowerResponseDto;
+import com.ssafy.jupging.service.ArticleService;
 import com.ssafy.jupging.service.FollowService;
 import com.ssafy.jupging.service.UserService;
 import io.swagger.annotations.ApiOperation;
@@ -11,6 +14,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 @CrossOrigin(origins = { "*" }, maxAge = 6000)
@@ -21,6 +25,8 @@ public class FollowController {
 
     private final FollowService followService;
     private final UserService userService;
+
+    private final ArticleService articleService;
 
     @ApiOperation(value = "팔로우 등록", notes = "팔로우 등록 성공 시 '팔로우 등록 성공' 반환 / 실패 시 에러메세지", response = ControllerResponse.class)
     @PostMapping
@@ -98,6 +104,38 @@ public class FollowController {
             followService.deleteByUserIdAndFollowUserId(userId, followUserId);
 
             response = new ControllerResponse("success", "팔로우 삭제 성공");
+        } catch (Exception e) {
+            response = new ControllerResponse("fail", e.getMessage());
+        }
+        return response;
+    }
+
+    @ApiOperation(value = "팔로우한 유저들의 게시글 찾기", notes = "성공시 게시글 리스트 반환 / 실패시 에러메시지", response = ControllerResponse.class)
+    @GetMapping("/findarticle/{id}")
+    public ControllerResponse findFollowArticle(@PathVariable("id") Long userId) {
+        ControllerResponse response = null;
+        try {
+            List<Follow> followList = followService.findFollow(userId); //팔로우한 사람들의 리스트를 받음
+            if (followList.isEmpty()) {
+                response = new ControllerResponse("success", null);
+            } else {
+                List<ArticleResponseDto> list = new ArrayList<>();
+                for (Follow foll : followList) {
+                    User user = userService.findUser(foll.getFollowUserId());
+                    FollowResponseDto followResponseDto = new FollowResponseDto();
+                    followResponseDto.saveIdAndNick(user.getUserId(), user.getNickname());
+
+                    List<Article> articleList = articleService.findByUserId(followResponseDto.getFollowUserId());
+                    for(Article article : articleList){
+                        list.add(new ArticleResponseDto(article));
+                    }
+                }
+
+                //전체 최신순으로 정렬
+                Collections.sort(list, (ArticleResponseDto o1, ArticleResponseDto o2) -> o2.getCreatedDate().compareTo(o1.getCreatedDate()));
+
+                response = new ControllerResponse("success", list);
+            }
         } catch (Exception e) {
             response = new ControllerResponse("fail", e.getMessage());
         }
