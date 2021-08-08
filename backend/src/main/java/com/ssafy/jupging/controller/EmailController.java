359aa0df9b84559faa406ef0b2a2cc5c795a8ac7
lee.sujeong10111 <lee.sujeong10111@gmail.com>
2021-08-08 16:08:59 +0900

    [BE] ADD : 이메일 swagger 추가

--- backend/src/main/java/com/ssafy/jupging/controller/EmailController.java
+++ backend/src/main/java/com/ssafy/jupging/controller/EmailController.java
@@ -4,6 +4,7 @@ import com.ssafy.jupging.domain.entity.User;
 import com.ssafy.jupging.dto.AuthorizationRequestDto;
 import com.ssafy.jupging.service.EmailService;
 import com.ssafy.jupging.service.UserService;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,23 +17,26 @@ public class EmailController {
     private final UserService userService;
     private final EmailService emailService;
 
+    @ApiOperation(value = "이메일 인증번호 전송", notes = "이메일 전송 성공시 '전송 성공' 반환 / 실패 시 에러메시지", response = ControllerResponse.class)
     @PostMapping("/auth/{email}")
     public ControllerResponse sendAuthEmail(@PathVariable String email){
         ControllerResponse response = null;
 
-        if(!userService.checkEmail(email))
+        if (!userService.checkEmail(email)) {
             return new ControllerResponse("fail", "이미 존재하는 이메일");
+        }
 
-        try{
+        try {
             emailService.sendAuthEmail(email);
-            response = new ControllerResponse("success", "인증 메일 전송 성공");
-        }catch (Exception e){
+            response = new ControllerResponse("success", "전송 성공");
+        } catch (Exception e) {
             response = new ControllerResponse("fail", e.getMessage());
         }
 
         return response;
     }
 
+    @ApiOperation(value = "이메일 인증번호가 맞는지 체크", notes = "성공시 '인증성공' 반환 / 실패 시 에러메시지", response = ControllerResponse.class)
     @PostMapping("/authcheck")
     public ControllerResponse checkAuthKey(@RequestBody AuthorizationRequestDto authorizationRequestDto){
         ControllerResponse response = null;
@@ -54,6 +58,7 @@ public class EmailController {
         return response;
     }
 
+    @ApiOperation(value = "임시 비밀번호 발급", notes = "임시비밀번호 발급 및 이메일 전송 성공시 '전송 성공' 반환 / 실패 시 에러메시지", response = ControllerResponse.class)
     @PostMapping("/changepw/{userId}")
     public ControllerResponse sendTempPwEmail(@PathVariable Long userId){
         ControllerResponse response = null;
@@ -62,7 +67,7 @@ public class EmailController {
 
         try{
             emailService.sendTempPwEmail(user);
-            response = new ControllerResponse("success", "임시 비밀번호 메일 전송 성공");
+            response = new ControllerResponse("success", "전송 성공");
         }catch (Exception e){
             response = new ControllerResponse("fail", e.getMessage());
         }
