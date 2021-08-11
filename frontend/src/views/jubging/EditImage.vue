8ab6a29bc2b36dc6ad2b75f290aed0f9071ef52d
문학일 <mhi0118@naver.com>
2021-08-11 16:08:51 +0900

    [FE] ADD: 줍깅 게시글

---
+++ frontend/src/views/jubging/EditImage.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <canvas id="canvas" width="400" height="300">
+
+    </canvas>
+  </div>
+</template>
+
+
+<script>
+
+export default {
+components:{
+},
+props: {
+},
+data() {
+  return {
+    canvas: null,
+    ctx: null,
+  }
+},
+computed:{
+},
+watch:{
+},
+created() {
+},
+mounted() {
+  this.canvas = document.getElementById("canvas")
+  var ctx = this.canvas.getContext("2d")
+
+  ctx.beginPath()
+  ctx.moveTo(3,5)
+  ctx.lineTo(100,16)
+  ctx.stroke()
+},
+methods:{
+},
+}
+</script>
+
+<style scoped>
+#canvas {
+  background-color: aqua;
+}
+</style>
