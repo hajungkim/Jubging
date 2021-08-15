6309e5f89487549c4d939b49b96157e7a96d318e
lee.sujeong10111 <lee.sujeong10111@gmail.com>
2021-08-15 11:58:33 +0900

    [BE] UPDATE : http 주소 변경

--- frontend/src/util/http-common.js
+++ frontend/src/util/http-common.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const HTTP = axios.create({
-  baseURL: `https://i5b207.p.ssafy.io/api`,
+  baseURL: `https://i5b207.p.ssafy.io/api`, 
   //baseURL: `http://localhost:8080/`,
   headers: {
     
