50429f3fdab6ff17eb025fa4e76093ff943b2d96
daeun <zerozero7bang@gmail.com>
2021-08-15 17:17:40 +0900

    [FE] UPDATE : 알람 모달

--- frontend/src/util/http-common.js
+++ frontend/src/util/http-common.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 export const HTTP = axios.create({
-  baseURL: `https://i5b207.p.ssafy.io/api`, 
-  //baseURL: `http://localhost:8080/`,
+  // baseURL: `https://i5b207.p.ssafy.io/api`, 
+  baseURL: `http://localhost:8080/`,
   headers: {
     
   }
