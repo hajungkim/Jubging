3b2e80b2ac56e9e4a3e7fdaa9100ce5271163a78
박선주 <cucu9823@naver.com>
2021-08-20 11:01:19 +0900

    사진삭제

---
+++ frontend/src/components/jubging/Explain.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <div class="explaing_img">
+      <img src="@/assets/iconlogo4.png" alt="" style="height:400px; width:400px;">
+      <span class="text">dsaadsa</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.explaing_img{
+  height:600px;
+  width:400px;
+  border:1px solid black;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.text{
+  margin-top:20px;
+}
+</style>
